f35dccdd9efe4ad4261ddd91ed5ca4478dbcab9c
Andre Bertuzzi <andreqbertuzzi@gmail.com>
2023-02-19 16:19:06 -0300

    Using KMS Encryption

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg')
+const AWS = require('aws-sdk')
+AWS.config.update({ region: 'sa-east-1' })
 
-const query = async (cmd, args) => {
-	return new Promise((resolve, reject) => {
-		const connection = new Pool()
+const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME
+const encrypted = process.env.CONNECTION_STRING
+let decrypted
 
-		console.log('connection created - going to connect ')
 
-		connection.query(cmd, args, (err, results) => {
-			console.log('query was ran')
-			connection.end((ef) => {
-				if (ef) {
-					console.log('connection end fails ', JSON.stringify(ef))
-				}
-				if (err) {
-					console.log('error ', JSON.stringify(err))
-					reject(err)
-				} else {
-					console.log('data ', JSON.stringify(results))
-					resolve(results)
-				}
-			})
-		})
-	})
+const query = async (event, connectionString) => {
+  const cmd = event.cmd
+  const args = event.args
+
+  console.log(connectionString, cmd, args)
+  const pool = new Pool({
+    connectionString,
+  })
+
+  try {
+    const results = await pool.query(cmd, args)
+    console.log('query was ran')
+    console.log('data ', JSON.stringify(results))
+    return results
+  } catch (err) {
+    console.log('error ', JSON.stringify(err))
+    throw err
+  } finally {
+    await pool.end()
+  }
 }
 
 exports.handler = async (event) => {
-	return query(event.command, event.args)
+  if (!decrypted) {
+    // Decrypt code should run once and variables stored outside of the
+    // function handler so that these are decrypted once per container
+    const kms = new AWS.KMS()
+    try {
+      const req = {
+        CiphertextBlob: Buffer.from(encrypted, 'base64'),
+        EncryptionContext: { LambdaFunctionName: functionName },
+      }
+      const data = await kms.decrypt(req).promise()
+      decrypted = data.Plaintext.toString('ascii')
+    } catch (err) {
+      console.log('Decrypt error:', err)
+      throw err
+    }
+  }
+  return await query(event, decrypted)
 }
