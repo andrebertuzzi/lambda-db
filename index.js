08174dcceb48c608f36c6c9592765bfea7d0fc1f
Andre Bertuzzi <andreqbertuzzi@gmail.com>
2023-03-15 17:04:12 -0300

    Hybrid invokation, sqs and directly

--- index.js
+++ index.js
@@ -7,19 +7,15 @@ const encrypted = process.env.CONNECTION_STRING
 let decrypted
 
 
-const query = async (event, connectionString) => {
-  const cmd = event.cmd
-  const args = event.args
-
-  console.log(connectionString, cmd, args)
+const query = async (cmd, args, connectionString) => {
+  console.log(cmd, args)
   const pool = new Pool({
     connectionString,
+    ssl: true,
   })
 
   try {
     const results = await pool.query(cmd, args)
-    console.log('query was ran')
-    console.log('data ', JSON.stringify(results))
     return results
   } catch (err) {
     console.log('error ', JSON.stringify(err))
@@ -46,5 +42,22 @@ exports.handler = async (event) => {
       throw err
     }
   }
-  return await query(event, decrypted)
+  if (Array.isArray(event.Records)) {
+    console.log('SQS FLOW')
+    // Event is an array of messages, for SQS trigger
+    const results = []
+    for (const message of event.Records) {
+      console.log('Body: ', message.body)
+      const { cmd, args } = JSON.parse(message.body)
+      const result = await query(cmd, args, decrypted)
+      results.push(result)
+    }
+    return results
+  } else {
+    console.log('DIRECT')
+    console.log(event)
+    // Event is an object, for direct invocation
+    const { cmd, args } = event
+    return await query(cmd, args, decrypted)
+  }
 }
